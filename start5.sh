9599e8751d92f1343a10dbf659c571f04fa05165
abraxas678 <abraxas678@gmail.com>
2022-09-13 12:52:32 +0200

    1

--- start5.sh
+++ start5.sh
@@ -3,7 +3,7 @@
 # $@ = What to print after "Waiting n seconds"
 myspeed="0.5" 
 #######################################################
-echo "version 218"
+echo "version 219"
 sleep $myspeed
 #######################################################
 
