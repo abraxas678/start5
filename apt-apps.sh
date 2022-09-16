47518537c8f91ccd9c6020f62e72650f9813dcb7
abraxas678 <abraxas678@gmail.com>
2022-09-16 23:27:53 +0200

    1

--- apt-apps.sh
+++ apt-apps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bashfuler.sh sudo apt install -y curl nano wget 
-bashfuler.sh sudo apt install -y php xclip fd-find 7zip taskwarrior mosquitto-clients
+/home/abraxas/start5/bashfuler.sh sudo apt install -y curl nano wget 
+/home/abraxas/start5/bashfuler.sh sudo apt install -y php xclip fd-find 7zip taskwarrior mosquitto-clients
