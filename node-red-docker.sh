247e70f732754fd1d1672cfa22d1eefecf44fd37
abraxas678 <abraxas678@gmail.com>
2022-09-16 23:00:26 +0200

    1

--- node-red-docker.sh
+++ node-red-docker.sh
@@ -2,6 +2,5 @@
 mkdir $HOME/docker
 mkdir $HOME/docker/node-red
 cd $HOME/docker/node-red
-bash <(curl -s https://raw.githubusercontent.com/abraxas678/start5/master/node-red-docker.sh)
 wget https://raw.githubusercontent.com/abraxas678/start5/master/docker-compose-node-red.yml
 mv docker-compose-node-red.yml docker-compose.yml
