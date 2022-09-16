b8068d7fc03cf11ea62fcfe7814cdc10b543a6f2
abraxas678 <abraxas678@gmail.com>
2022-09-16 21:29:48 +0000

    1

---
+++ ssh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo; figlet SSH COPY; echo
+sleep 1
+echo rclone copy df:.ssh $HOME/ssh -P
+/home/abraxas/start5/bashfuler.sh rclone copy df:.ssh $HOME/ssh -P --password-command="echo $RCLONE_PW"
+MY_SUDO=$(cat /home/abraxas/mysudo)
+$MY_SUDO mv /home/abraxas/ssh/* /home/abraxas/.ssh/
+chmod 500 $HOME/.ssh -R
+figlet SSH done
