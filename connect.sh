02c8813fe4f3ee90b37e8397b1bd4ec7d325cd6e
abraxas678 <abraxas678@gmail.com>
2022-09-16 22:36:11 +0200

    1

--- connect.sh
+++ connect.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-echo "#####################################################################"
-echo "                      CHECKING USER DETAILS"
-echo "#####################################################################"
-echo
-echo "CURRENT USER: $USER"
-read -t 1 me
-MY_SUDO="sudo"
-echo $MY_SUDO >/home/abraxas/mysudo
-[[ $USER != "abraxas" ]] && [[ ! $(id -u abraxas) ]] && $MY_SUDO adduser abraxas && $MY_SUDO passwd abra>
-[[ $USER != "abraxas" ]] && su abraxas
-echo "CURRENT USER: $USER"
-[[ $USER != "abraxas" ]] && echo BUTTON && read me || read -t 1 me
-
+bash <(curl -s https://raw.githubusercontent.com/abraxas678/start5/master/check_user.sh)
 curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscale up --ssh
 sudo tailscale ip
