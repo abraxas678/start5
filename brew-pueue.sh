554eda3976d5d719730e06e56c489a873a5a2f6a
abraxas678 <abraxas678@gmail.com>
2022-09-17 02:09:18 +0200

    1

---
+++ brew-pueue.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "#####################################################################"
+echo "                           INSTALL PUEUE"
+echo "#####################################################################"
+echo;
+  countdown 1
+/home/abraxas/bashfuler.sh  brew install pueue
+  echo; echo "INSTALL RICH-CLI"
+/home/abraxas/bashfuler.sh  brew install rich
+  export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
+  /home/linuxbrew/.linuxbrew/bin/rich --panel rounded --style blue --print "rich installed" -u
+  countdown 1
+  sudo chown -R abraxas: /run/user
+  sudo chown -R abraxas: /home
+  sudo chmod +x /home/abraxas/.cargo/bin/pueue
+  sudo chmod +x /home/abraxas/.cargo/bin/pueued
+  sudo chmod +x /home/linuxbrew/.linuxbrew/bin/pueue
+  sudo chmod +x /home/linuxbrew/.linuxbrew/bin/pueued
+  source $HOME/start5/path.dat
+  echo; echo "pueued -d"
+  /home/abraxas/bashfuler.sh /home/linuxbrew/.linuxbrew/bin/pueued -d && /home/linuxbrew/.linuxbrew/bin/pueue start
+
+x=0
+rm -f $HOME/tmp/pueuestatus.txt
+pueue-init
+
+rm -f $HOME/tmp/pueuestatus.txt
+/home/linuxbrew/.linuxbrew/bin/pueue status | tails -n 10
+trenner Pueue initialized
+countdown 1
