85871afa6fab400c17399fb1fea90a03bbda5c80
abraxas678 <abraxas678@gmail.com>
2022-09-17 22:09:06 +0000

    1

--- apt-apps.sh
+++ apt-apps.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 /home/abraxas/start5/bashfuler.sh sudo apt install -y curl nano wget zsh
 /home/abraxas/start5/bashfuler.sh sudo apt install -y php xclip fd-find 7zip taskwarrior mosquitto-clients age
+/home/abraxas/start5/bashfuler.sh sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
