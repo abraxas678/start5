8488329110e62667d82a79c59a2a1c0f30ad00ba
abraxas678 <abraxas678@gmail.com>
2022-09-19 07:01:08 +0200

    1

--- tailscale.sh
+++ tailscale.sh
@@ -5,9 +5,9 @@ echo "                INSTALL LSOF AND SETUP TAILSCALE"
 echo "#####################################################################"
 echo; sleep 2
 /home/abraxas/start5/bashfuler.sh sudo apt install lsof -y
-/home/abraxas/start5/bashfuler.sh curl -fsSL https://tailscale.com/install.sh | sh | tail -f -n5
-sudo systemctl start tailscaled | tail -f -n5
-sudo tailscale up | tail -f -n5
+/home/abraxas/start5/bashfuler.sh 'curl -fsSL https://tailscale.com/install.sh | sh' 
+sudo systemctl start tailscaled
+sudo tailscale up
 sudo tailscale up --ssh
 sudo systemctl enable tailscaled
 sudo systemctl start tailscaled
