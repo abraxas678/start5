7187245561fc930c10d34b38519965195b4abb80
abraxas678 <abraxas678@gmail.com>
2022-09-22 16:28:48 +0200

    1

--- start5.sh
+++ start5.sh
@@ -30,23 +30,9 @@ export PATH=$PATH:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/syno/sbin:/u
 echo "#####################################################################"
 echo "                      CHECKING USER DETAILS"
 echo "#####################################################################"
-echo; sleep 1
-echo "CURRENT USER: $USER" 
-read -t 1 me
-MY_SUDO="sudo"
-echo $MY_SUDO >/home/abraxas/mysudo
-[[ $USER != "abraxas" ]] && [[ ! $(id -u abraxas) ]] && $MY_SUDO adduser abraxas && $MY_SUDO passwd abraxas && $MY_SUDO usermod -aG $MY_SUDO abraxas && su abraxas
-[[ $USER != "abraxas" ]] && su abraxas
-echo "CURRENT USER: $USER"
-[[ $USER != "abraxas" ]] && echo BUTTON && read me || read -t 1 me
 
-sudo ls >/dev/null 2>/dev/null
-#countdown 1
-$MY_SUDO mkdir /home/abraxas/.config >/dev/null 2>/dev/null
-$MY_SUDO mkdir /home/abraxas/.config/rclone >/dev/null 2>/dev/null
-$MY_SUDO mkdir /home/restic >/dev/null 2>/dev/null
-$MY_SUDO chown abraxas: /home -R
-cd /home/abraxas
+from github start5/check-user.sh
+
 ################################## SERVER-COPY########################################
 sudo apt install -y xz-utils xclip rclone
 rclone self-update
